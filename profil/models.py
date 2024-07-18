74a52dfecc77ef72121dfc3b75733749e9c17b78
ramirokaffo <ramirokaffo>
2024-07-18 14:03:39 +0100

    Cmd physiques

--- profil/models.py
+++ profil/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class UserProfilModel(models.Model):
     user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, blank=True, verbose_name="propriétaire du profil")
     last_updated = models.DateTimeField(auto_now=True, verbose_name="dernière mise à jour")
-    created_at = models.DateField(blank=True, auto_now=True, null=True, verbose_name="créé le")
+    created_at = models.DateTimeField(blank=True, auto_now=True, null=True, verbose_name="créé le")
     profil_image = models.ImageField(blank=True, null=True, upload_to='images/profil', verbose_name="photo de profil")
 
     class Meta:
