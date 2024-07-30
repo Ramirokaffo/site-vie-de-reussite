bfa1f5fc2e7d7e3046b9dacc8db8f4da65dac131
ramirokaffo <ramirokaffo>
2024-07-30 23:50:46 +0100

    Integration des liens vers tous les reseaux

--- blog/views.py
+++ blog/views.py
@@ -88,13 +88,13 @@ def comment(request: WSGIRequest, post_id: int):
     if request.POST:
         content: str = request.POST.get("content")
         post = BlogPost.objects.get(id=post_id)
-        print("le profil de l'utilisateur: ")
-        print(request.session.load())
+        # print("le profil de l'utilisateur: ")
+        # print(request.session.load())
         user_profil_session = request.session.get("user_profile")
-        print(user_profil_session)
+        # print(user_profil_session)
         # user_profil_id = user_profil_session.get("id")
         user_profil = UserProfilModel.objects.get(user__id=request.user.id)
-        print(user_profil)
+        # print(user_profil)
 
         new_comment = BlogComment.objects.create(content=content, post=post, author=user_profil)
         new_comment.save()
